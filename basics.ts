07ad936dad714d2ad844c3f4c573f94068bbebf9
arvind <arvind@clevertap.com>
2021-07-31 13:00:58 +0530

    Added Typesript Module

---
+++ basics.ts
@@ -0,0 +1,26 @@
+function add(n1: number, n2: number, showResult: boolean, resultPhrase: string) {
+    const result = n1 + n2
+  if (showResult) {
+    console.log(resultPhrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 3;
+const number2 = 7.2;
+const printResult = true;
+const resultPhrase= "Result is : "
+
+//Type Inference -> Throws error, ts auto detect type of assigned values to variables
+
+// let a : number;
+// a = "2";
+
+// let ans = 'String Stored'
+// ans = 3;
+
+
+
+const res = add(number1, number2, printResult, resultPhrase);
+console.log(res);
